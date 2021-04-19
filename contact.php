cd6a3732efe24072048460ce80a9690ccee36e8e
Your Name <you@example.com>
2021-04-18 22:33:04 -0400

    change copyright

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@ if(!empty($_POST["send"])) {
 	$subject = $_POST["subject"];
 	$content = $_POST["content"];
 
-	$toEmail = "admin@phppot_samples.com";
+	$toEmail = "juliapurza@gmail.com";
 	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
 	if(mail($toEmail, $subject, $content, $mailHeaders)) {
 	    $message = "Your contact information is received successfully.";
@@ -13,4 +13,12 @@ if(!empty($_POST["send"])) {
 	}
 }
 require_once "contact-view.php";
+?>
+
+<?php
+$conn = mysqli_connect("localhost", "root", "test", "blog_samples") or die("Connection Error: " . mysqli_error($conn));
+mysqli_query($conn, "INSERT INTO tblcontact (user_name, user_email,subject,content) VALUES ('" . $name. "', '" . $email. "','" . $subject. "','" . $content. "')");
+$insert_id = mysqli_insert_id($conn);
+if(!empty($insert_id)) {
+$message = "Your contact information is saved successfully";
 ?>
